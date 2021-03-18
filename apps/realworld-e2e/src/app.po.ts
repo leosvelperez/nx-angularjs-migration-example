2c46b164c2f49a4433e640e50cabd3adcab41454
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-03-18 16:29:27 +0000

    add angular app, migrate angularjs deps

---
+++ apps/realworld-e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  async navigateTo(): Promise<unknown> {
+    return browser.get(browser.baseUrl);
+  }
+
+  async getTitleText(): Promise<string> {
+    return element(by.css('nx-migrate-angularjs-root header h1')).getText();
+  }
+}
